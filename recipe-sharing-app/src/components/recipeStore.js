1ca19801f0894798f147157a546a96eff47d4dd6
sunie-eng <odoh619@gmail.com>
2025-07-30 01:20:52 -0700

    Add searchTerm and setSearchTerm to recipeStore

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,17 +1,27 @@
 import { create } from 'zustand';
 
-export const useRecipeStore = create((set) => ({
+const useRecipeStore = create((set) => ({
   recipes: [],
+  searchTerm: '',  // ✅ NEW
+
   addRecipe: (newRecipe) =>
     set((state) => ({ recipes: [...state.recipes, newRecipe] })),
+
   deleteRecipe: (id) =>
     set((state) => ({
       recipes: state.recipes.filter((recipe) => recipe.id !== id),
     })),
+
   updateRecipe: (updatedRecipe) =>
     set((state) => ({
       recipes: state.recipes.map((recipe) =>
         recipe.id === updatedRecipe.id ? updatedRecipe : recipe
       ),
     })),
+
+  setRecipes: (recipes) => set({ recipes }),
+
+  setSearchTerm: (term) => set({ searchTerm: term }),  // ✅ NEW
 }));
+
+export default useRecipeStore;
