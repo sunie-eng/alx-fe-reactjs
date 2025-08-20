774f76a25a83ff43c55b97c94a399695f1320980
sunie-eng <odoh619@gmail.com>
2025-08-20 12:14:51 -0700

    Setup React with Tailwind for recipe-sharing-platform

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -1,11 +1,12 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
-    "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./index.html",              
+    "./public/index.html",       
+    "./src/**/*.{js,jsx,ts,tsx}" 
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
